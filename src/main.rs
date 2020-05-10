7f5461c799325138fe11028da3dd4ca217f25695
Marcin Górny <marcin@squirro.com>
2020-05-10 09:43:15 +0200

    serialize id and version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use serde::{Serialize, Deserialize};
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct KeyStore {
+    version: u8,
+    id: String
+}
+
+fn serialize(store: &KeyStore ) {
+    let serialized: String = serde_json::to_string(&store).unwrap();
+    println!("serialized = {}", serialized);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let uuid:String = Uuid::new_v4().to_hyphenated().to_string();
+    let store = KeyStore { version: 1, id: uuid };
+    serialize(&store);
 }
